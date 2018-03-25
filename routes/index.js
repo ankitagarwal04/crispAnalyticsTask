d72f51a4c2defe296dd7130f6a1ae782db96cd9c
ankitagarwal04 <ankit.mrka4@gmail.com>
2018-03-25 21:57:23 +0530

    integrated react with expressjs

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // Comment out this line:
+  res.send('respond with a resource');
+});
+
+module.exports = router;
